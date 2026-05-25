ff16b4f10b69427edef148b2c50403b04b727d79
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 15:36:02 +0200

    Update SLURM job parameters and output log format

--- scripts/annotation/04.filter_annotation/09_annot_stats.sh
+++ scripts/annotation/04.filter_annotation/09_annot_stats.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X_XX
 #SBATCH -p pelle
 #SBATCH -c 1
 #SBATCH -t 00:30:00
 #SBATCH -J annot_stats
-#SBATCH -o 20260116_AGAT_stats.log
+#SBATCH -o 20YYMMDD_AGAT_stats.log
 #SBATCH --mail-type=END
 
 mamba activate agat
 
-WDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/06.filtered/08.final
+WDIR=/path/to/final
 
 agat_sp_statistics.pl -gff ${WDIR}/A_najas_filt_final.gff -o ${WDIR}/filt_annot_stats.txt
