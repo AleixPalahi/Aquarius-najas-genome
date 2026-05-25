522a53dffe0953e26ce318a5b5403a5a39c591b6
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 13:48:35 +0200

    Modify paths and job parameters in interpromerge.sh

--- scripts/annotation/03.func_annotation/04_interpromerge.sh
+++ scripts/annotation/03.func_annotation/04_interpromerge.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p pelle
 #SBATCH -n 8
 #SBATCH -t 08:00:00
 #SBATCH -J InterProMerge
-#SBATCH -o 20251211_InterProMerge.log
+#SBATCH -o 20YYMMDD_InterProMerge.log
 #SBATCH --mail-type=END
 
 #Merge interproscan output into braker annotation
 
-# Load environment (AGAT v 1.5.1)
-mamba activate agat
+# Load environment (AGAT v 1.5.1) before running
+# mamba activate agat
 
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/01.braker/braker.gff
-IP_FILE=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/01.interpro/01.output/braker_nostop.faa.tsv
-OUTDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/01.interpro/01.output
+GFF=/path/to/braker.gff
+IP_FILE=/path/to/braker_nostop.faa.tsv
+OUTDIR=/path/to/01.interpro/01.output
 
 agat_sp_manage_functional_annotation.pl -f $GFF -i $IP_FILE -o ${OUTDIR}/braker_interpro
