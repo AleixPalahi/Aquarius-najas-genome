b2f0d3c6951b7c0cbdfd81e29264626dd262732c
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 14:04:44 +0200

    Update paths and merge commands in 07_merge_eggnog.sh

--- scripts/annotation/03.func_annotation/07_merge_eggnog.sh
+++ scripts/annotation/03.func_annotation/07_merge_eggnog.sh
@@ -13,16 +13,12 @@
 # Load environment (AGAT v 1.5.1)
 mamba activate agat
 
-WDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional
+WDIR=/path/to/functional
 
-agat_sp_merge_annotations.pl --gff ${WDIR}/04.final_annotations/01_braker_interpro_blast_ID/braker.gff \
-	--gff ${WDIR}/03.eggnog/03.diamond/A_najas_eggnog_diamond.emapper.decorated.gff \
-       	--out ${WDIR}/04.final_annotations/02_interpro_blast_diamond/interpro_blast_diamond.gff
+agat_sp_merge_annotations.pl --gff ${WDIR}/04.final_annotation/braker_interpro_blast_ID/braker.gff \
+	--gff ${WDIR}/03.eggnog/02.diamond/A_najas_eggnog_diamond.emapper.decorated.gff \
+       	--out ${WDIR}/04.final_annotation/interpro_blast_diamond/interpro_blast_diamond.gff
 
-agat_sp_merge_annotations.pl --gff ${WDIR}/04.final_annotations/02_interpro_blast_diamond/interpro_blast_diamond.gff \
+agat_sp_merge_annotations.pl --gff ${WDIR}/04.final_annotation/interpro_blast_diamond/interpro_blast_diamond.gff \
         --gff ${WDIR}/03.eggnog/01.mm_HS/A_najas_eggnog.emapper.decorated.gff \
-        --out ${WDIR}/04.final_annotations/03_interpro_blast_diamond_mmseqs/interpro_blast_diamond_mmseqs.gff
-
-agat_sp_merge_annotations.pl --gff ${WDIR}/04.final_annotations/02_interpro_blast_diamond/interpro_blast_diamond.gff \
-        --gff ${WDIR}/03.eggnog/02.mm_def/A_najas_eggnog_mm_def.emapper.decorated.gff \
-        --out ${WDIR}/04.final_annotations/04_interpro_blast_diamond_mmseqsdef/interpro_blast_diamond_mmseqsdef.gff
+        --out ${WDIR}/04.final_annotation/interpro_blast_diamond_mmseqs/interpro_blast_diamond_mmseqs.gff
