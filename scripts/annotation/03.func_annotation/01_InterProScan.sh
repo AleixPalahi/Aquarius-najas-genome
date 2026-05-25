b3759bc381c95d430187ccfb635f08b9c8223b4f
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 11:58:34 +0200

    Update paths and SLURM parameters in InterProScan script

--- scripts/annotation/03.func_annotation/01_InterProScan.sh
+++ scripts/annotation/03.func_annotation/01_InterProScan.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -l
 
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p core
 #SBATCH -n 8
 #SBATCH -t 20:00:00
 #SBATCH -J interproscan
-#SBATCH -o 20251125_interproscan.log
+#SBATCH -o 20YYMMDD_interproscan.log
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ingo.mueller@ebc.uu.se
 
 # Load modules
 module load bioinfo-tools InterProScan/5.62-94.0
 
-InFAA=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/01.interpro/00.input/braker_nostop.faa
-OutDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/01.interpro/01.output/
+InFAA=/path/to/braker_nostop.faa
+OutDIR=/path/to/01.interpro/01.output/
 
 interproscan.sh -i $InFAA -cpu 8 -t p -dp -pa --goterms --iprlookup -d $OutDIR
