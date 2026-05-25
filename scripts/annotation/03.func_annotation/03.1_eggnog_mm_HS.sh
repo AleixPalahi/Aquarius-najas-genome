78d04635c273ffe19fa7b826dd9924fdf39383d8
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 13:45:08 +0200

    Modify SLURM script for eggnog-mapper paths

--- scripts/annotation/03.func_annotation/03.1_eggnog_mm_HS.sh
+++ scripts/annotation/03.func_annotation/03.1_eggnog_mm_HS.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -l
 
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p core
 #SBATCH -n 16
 #SBATCH -t 03:00:00
 #SBATCH -J eggnog_mm_HS
-#SBATCH -o 20251127_eggnog_mm_HS.log
+#SBATCH -o 20YYMMDD_eggnog_mm_HS.log
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ingo.mueller@ebc.uu.se
 
 #Run eggnog-mapper with mmseqs2 and higher than default sensitivity settings
 #max sensitivity taken from Kallenborn, et al. (2025) GPU-accelerated homology search with MMseqs2. Nature Methods
@@ -15,9 +14,9 @@
 # Load modules
 module load bioinfo-tools eggNOG-mapper/2.1.9 eggNOG_data/5.0.0
 
-InFAA=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/01.interpro/00.input/braker_nostop.faa
-OutDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/03.eggnog/01.mm_HS
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/01.braker/braker.gff
+InFAA=/path/to/braker_nostop.faa
+OutDIR=/path/to/mm_HS
+GFF=/path/to/braker.gff
 
 #Use/Make scratch dir
 export scratchDIR=${SNIC_TMP}/A_najas_eggnog
