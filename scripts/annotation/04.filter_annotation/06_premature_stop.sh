dcee2d7793f09c5cc09de494fcb7a3996dd28860
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 15:27:22 +0200

    Modify SLURM script for premature stop annotation

--- scripts/annotation/04.filter_annotation/06_premature_stop.sh
+++ scripts/annotation/04.filter_annotation/06_premature_stop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p pelle
 #SBATCH -c 1
 #SBATCH -t 00:30:00
 #SBATCH -J premStop
-#SBATCH -o 20260113_premStop.log
+#SBATCH -o 20YYMMDD_premStop.log
 #SBATCH --mail-type=END
 
 mamba activate agat
 
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/04.merged_annotations/04_interpro_blast_diamond_mmseqsHS_FINAL_MERGED/interpro_blast_diamond_mmseqs.gff
-GENOME=/proj/snic2019-35-58/water_strider/ingo/data/raw_internal/01.ref/Anajas_masked_genome.fa
-OUTDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/06.filtered/06.premature_stop
+GFF=/path/to/interpro_blast_diamond_mmseqs.gff
+GENOME=/path/to/masked_genome.fa
+OUTDIR=/path/to/06.premature_stop
 
 agat_sp_flag_premature_stop_codons.pl -gff $GFF -f $GENOME --ct 1 -o ${OUTDIR}/A_najas_premStop.gff
