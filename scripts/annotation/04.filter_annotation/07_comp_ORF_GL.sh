cfc48f8ea836870f952252603013d816f21e1bee
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 15:28:45 +0200

    Update SLURM script paths and job parameters

--- scripts/annotation/04.filter_annotation/07_comp_ORF_GL.sh
+++ scripts/annotation/04.filter_annotation/07_comp_ORF_GL.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p pelle
 #SBATCH -c 1
 #SBATCH -t 00:30:00
 #SBATCH -J comp_ORF_GL
-#SBATCH -o 20260115_comp_ORF_GL.log
+#SBATCH -o 20YYMMDD_comp_ORF_GL.log
 #SBATCH --mail-type=END
 
 mamba activate agat
 
-WDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/06.filtered
-OUTDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/06.filtered/07.comp_stat
+WDIR=/path/to/filter_dir
+OUTDIR=/path/to/07.comp_stat
 
 #ORF50 v GL150
 agat_sp_compare_two_annotations.pl -gff1 ${WDIR}/01.ORF_size/A_najas_ORF_50_NOT_sup50.gff \
