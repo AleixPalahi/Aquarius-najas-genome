3c79ab25b1642701014290a67a1cc5158badd3c5
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 11:51:46 +0200

    Update paths and job parameters in gtf2fa script

--- scripts/annotation/02.gene_annotation/02_gtf2fa.sh
+++ scripts/annotation/02.gene_annotation/02_gtf2fa.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 05:00:00
 #SBATCH -J aquarius_gtf2fa
-#SBATCH -o 20251117_gtf2fa.log
+#SBATCH -o 20YYMMDD_gtf2fa.log
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ingo.mueller@ebc.uu.se
 
 module load bioinfo-tools cufflinks/2.2.1
 
-GTF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/A_najas/braker/braker.gtf
-REF=/proj/snic2019-35-58/water_strider/ingo/data/raw_internal/01.ref/Anajas_masked_genome.fa
-OUT=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/01.QC/01.busco/gtf_transcriptome.fasta
+GTF=/path/to/braker.gtf
+REF=/path/to/masked_genome.fa
+OUT=/path/to/gtf_transcriptome.fasta
 
 gffread -x $OUT -g $REF $GTF
