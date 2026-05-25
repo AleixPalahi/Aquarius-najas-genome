8ae026f3835908751f0f2bf4ecc740ae210d09f9
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 15:26:37 +0200

    Update SLURM directives and file paths in script

--- scripts/annotation/04.filter_annotation/05_overlap.sh
+++ scripts/annotation/04.filter_annotation/05_overlap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p pelle
 #SBATCH -c 1
 #SBATCH -t 00:30:00
 #SBATCH -J overlap
-#SBATCH -o 20260113_OL.log
+#SBATCH -o 20YYMMDD_OL.log
 #SBATCH --mail-type=END
 
 mamba activate agat
 
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/04.merged_annotations/04_interpro_blast_diamond_mmseqsHS_FINAL_MERGED/interpro_blast_diamond_mmseqs.gff
-OUTDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/06.filtered/05.overlap
+GFF=/path/to/interpro_blast_diamond_mmseqs.gff
+OUTDIR=/path/to/05.overlap
 
 agat_sp_fix_overlaping_genes.pl -f $GFF -m -o ${OUTDIR}/A_najas_OL.gff
