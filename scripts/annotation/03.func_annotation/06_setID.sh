ec45660521e838b587f4cc38d3a2312f457223a9
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 13:52:15 +0200

    Modify job script for setID with new paths

--- scripts/annotation/03.func_annotation/06_setID.sh
+++ scripts/annotation/03.func_annotation/06_setID.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p pelle
 #SBATCH -n 1
 #SBATCH -t 01:00:00
 #SBATCH -J setID
-#SBATCH -o 20251212_setID.log
+#SBATCH -o 20YYMMDD_setID.log
 #SBATCH --mail-type=END
 
 ### Set IDs for merged gff
@@ -12,8 +12,7 @@
 # Load environment (AGAT v 1.5.1)
 mamba activate agat
 
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/02.BLAST/braker_interpro_blast/braker.gff
-WDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/04.final_annotation
-
+GFF=/path/to/braker_interpro_blast/braker.gff
+WDIR=/path/to/final_annotation
 
 agat_sp_manage_functional_annotation.pl -f $GFF --ID A_najas -o ${WDIR}/braker_interpro_blast_ID
