7a5f045ce2572d43ff1324c66bacf9a32fa50e2f
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 13:46:49 +0200

    Modify eggnog_diamond.sh with new paths

--- scripts/annotation/03.func_annotation/03.2_eggnog_diamond.sh
+++ scripts/annotation/03.func_annotation/03.2_eggnog_diamond.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -l
 
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p core
 #SBATCH -n 16
 #SBATCH -t 03:00:00
 #SBATCH -J eggnog_diamond
-#SBATCH -o 20251127_eggnog_diamond.log
+#SBATCH -o 20YYMMDD_eggnog_diamond.log
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ingo.mueller@ebc.uu.se
 
 #Run eggnog-mapper with diamond, running with iterations similar to mmseqs2 with the final iteration on ultra-sensitive settings which should be slightly better than
 #mmseqs2 at s=7.5 (Buchfink, et al. (2025). Sensitive protein alignments at tree-of-life scale using DIAMOND. Nature methods)
@@ -15,9 +14,9 @@
 # Load modules
 module load bioinfo-tools eggNOG-mapper/2.1.9 eggNOG_data/5.0.0
 
-InFAA=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/01.interpro/00.input/braker_nostop.faa
-OutDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/03.eggnog/03.diamond
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/01.braker/braker.gff
+InFAA=/path/to/braker_nostop.faa
+OutDIR=/path/to/diamond
+GFF=/path/to/braker.gff
 
 #Use/Make scratch dir
 export scratchDIR=${SNIC_TMP}/A_najas_eggnog_diamond
