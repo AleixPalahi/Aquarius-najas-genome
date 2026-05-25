f46b9e9e72c3830e5b052c21d33503ae254c518e
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 11:52:38 +0200

    Update script paths and job parameters

--- scripts/annotation/02.gene_annotation/03_busco.sh
+++ scripts/annotation/02.gene_annotation/03_busco.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -l
-
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p core
 #SBATCH -n 10
 #SBATCH -t 4:00:00
 #SBATCH -J aquarius_busco
-#SBATCH -o 20251117_busco.log
+#SBATCH -o 20YYMMDD_busco.log
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ingo.mueller@ebc.uu.se
 
 
 # Load modules
@@ -15,8 +13,8 @@ module load bioinfo-tools
 module load augustus/3.5.0
 module load BUSCO/5.7.1
 
-INFILE=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/01.QC/01.busco/gtf_transcriptome.fasta
-OUTFILE=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/01.QC/01.busco/A_najas_busco
+INFILE=/path/to/gtf_transcriptome.fasta
+OUTFILE=/path/to/A_najas_busco
 
 source $AUGUSTUS_CONFIG_COPY
 busco -i $INFILE -o $OUTFILE -l $BUSCO_LINEAGE_SETS/hemiptera_odb10/ -m tran -c 10 -f 
