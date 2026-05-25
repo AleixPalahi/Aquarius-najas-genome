5efe00a3494360ee6577d3004449b49c021309d4
Ingo Achim Müller <82661109+IngoMue@users.noreply.github.com>
2026-05-25 15:29:35 +0200

    Modify SLURM parameters and update file paths
    Updated SLURM job parameters and file paths in the script.

--- scripts/annotation/04.filter_annotation/08_all_filters.sh
+++ scripts/annotation/04.filter_annotation/08_all_filters.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -A uppmax2025-2-17
+#SBATCH -A uppmax20XX-X-XX
 #SBATCH -p pelle
 #SBATCH -c 1
 #SBATCH -t 00:30:00
 #SBATCH -J Filter_annot
-#SBATCH -o 20260116_filterannot.log
+#SBATCH -o 20YYMMDD_filterannot.log
 #SBATCH --mail-type=END
 
 mamba activate agat
 
-GFF=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/04.merged_annotations/04_interpro_blast_diamond_mmseqsHS_FINAL_MERGED/interpro_blast_diamond_mmseqs.gff
-GENOME=/proj/snic2019-35-58/water_strider/ingo/data/raw_internal/01.ref/Anajas_masked_genome.fa
-WDIR=/proj/snic2019-35-58/water_strider/ingo/data/intermediate/nobackup/02.annotation/03.functional/06.filtered/08.final/
+GFF=/path/to/interpro_blast_diamond_mmseqs.gff
+GENOME=/path/to/masked_genome.fa
+WDIR=/path/to/final/
 
 #Filter by ORF size (>= 50 aa)
 agat_sp_filter_by_ORF_size.pl -g $GFF -s 50 -t ">=" -o ${WDIR}/A_najas_filt_ORF.gff
